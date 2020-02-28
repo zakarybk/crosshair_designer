dbafc5bb0449eede4a86c9c3845dbcb03b58df67
zakarybk <zakarybk@users.noreply.github.com>
2020-02-28 23:32:42 +0000

    Created new convar system based on old so backwards compatible but a little more flexible + added limits to what the vars can be set to through console

--- lua/crosshair-designer/db.lua
+++ lua/crosshair-designer/db.lua
@@ -5,3 +5,104 @@ end
 CrosshairDesigner.Load = function(crossID)
 
 end
+
+local convars = {}
+
+CrosshairDesigner.SetUpConvars = function(convars)
+	for i, convarData in pairs(convars) do
+		CrosshairDesigner.AddConvar(convarData)
+		CrosshairDesigner.AddConvarCallback(convarData)
+	end
+end
+
+CrosshairDesigner.AddConvar = function (convarData)
+	convars[convarData.id] = {}
+	convars[convarData.id].data = convarData
+	convars[convarData.id].var = CreateClientConVar(
+		convarData.id, 
+		convarData.default,
+		true, 
+		false,
+		convarData.help or nil,
+		convarData.min or nil,
+		convarData.max or nil
+	)
+end
+
+-- Verify convars edited by user
+CrosshairDesigner.AddConvarCallback = function(convarData)
+	cvars.AddChangeCallback(
+		convarData.id,
+		function(convarName, oldVal, newVal)
+			
+			newVal = CrosshairDesigner.ClampConvar(convarData, oldVal, newVal)
+
+			hook.Run("CrosshairDesigner_ValueChanged", 
+				convarData.id, 
+				tostring(oldVal), 
+				tostring(newVal)
+			)
+		end,
+		"CrosshairDesigner." .. convarData.id
+	)
+end
+
+CrosshairDesigner.ClampConvar = function(convarData, oldVal, newVal)
+	if convarData.isBool then -- bool
+
+		if tobool(newVal) == nil then
+			if tobool(oldVal) == nil then
+				newVal = convarData.default
+			else
+				newVal = oldVal
+			end
+			CrosshairDesigner.SetValue(convarData.id, newVal)
+		end
+		
+	elseif tonumber(convarData.default) != nil then -- number
+
+		if tonumber(newVal) == nil then
+			if tonumber(oldVal) == nil then
+				newVal = convarData.default
+			else
+				newVal = oldVal
+			end
+			CrosshairDesigner.SetValue(convarData.id, newVal)
+		else
+			local clamped = tonumber(newVal)
+
+			if convarData.min != nil then
+				clamped = math.max(clamped, convarData.min)
+			end
+
+			if convarData.max != nil then
+				clamped = math.min(clamped, convarData.max)
+			end
+
+			if clamped != tonumber(newVal) then
+				CrosshairDesigner.SetValue(convarData.id, clamped)
+				newVal = clamped
+			end
+		end
+	end
+
+	return newVal
+end
+
+CrosshairDesigner.GetValue = function(id)
+	return (convars[id] != nil and convars[id].var:GetString()) or "0"
+end
+
+CrosshairDesigner.SetValue = function(id, val)
+	RunConsoleCommand(convars[id].data.id, val)
+end
+
+CrosshairDesigner.GetLimitMin = function(id)
+	return (convars[id] != nil and convars[id].data.min) or 0
+end
+
+CrosshairDesigner.GetLimitMax = function(id)
+	return (convars[id] != nil and convars[id].data.max) or 0
+end
+
+CrosshairDesigner.GetConvars = function() return convars end
--- lua/crosshair-designer/load.lua
+++ lua/crosshair-designer/load.lua
@@ -10,4 +10,181 @@ else
 	include("db.lua")
 	include("draw.lua")
 	include("menu.lua")
+
+	/*
+	local convars = {}
+	convars.ShowHL2 = CreateClientConVar("toggle_crosshair_hide", 0, true, false, "Show the Half Life crosshair", 0, 1)
+	convars.HideCross = CreateClientConVar("toggle_crosshair", 1, true, false)
+	convars.HideOnADS = CreateClientConVar("cross_ads", 1, true, false)
+	convars.UseLine = CreateClientConVar("cross_line", 1, true, false)
+	convars.ColOnTarget = CreateClientConVar("hc_target_colour", 1, true, false)
+	convars.Dynamic = CreateClientConVar("hc_dynamic_cross", 1, true, false)
+	convars.HideInVeh = CreateClientConVar("hc_vehicle_cross", 1, true, false)
+	convars.
+	*/
+
+	CrosshairDesigner.SetUpConvars({
+		["ShowHL2"] = {
+			id="toggle_crosshair_hide", 
+			default="1", 
+			help="Show the half life crosshair", 
+			isBool=true
+		},
+		["HideCross"] = {
+			id="toggle_crosshair", 
+			default="0", 
+			help="Hide the custom crosshair", 
+			isBool=true
+		},
+		["HideOnADS"] = {
+			id="cross_ads", 
+			default="1", 
+			help="Hide the custom crosshair when aiming down sights", 
+			isBool=true
+		},
+		["UseLine"] = {
+			id="cross_line", 
+			default="1", 
+			help="Use the line style for the crosshair", 
+			isBool=true
+		},
+		["ColOnTarget"] = {
+			id="hc_target_colour", 
+			default="1", 
+			help="Use the line style for the crosshair", 
+			isBool=true
+		},
+		["Dynamic"] = {
+			id="hc_dynamic_cross",
+			default="0",
+			help="Make the crosshair dynamic",
+			isBool=true
+		},
+
+		["Red"] = {
+			id="cross_hud_color_r",
+			default="29",
+			help="Change the amount of red in the crosshair",
+			min=0,
+			max=255,
+		},
+		["Green"] = {
+			id="cross_hud_color_g",
+			default="0",
+			help="Change the amount of green in the crosshair",
+			min=0,
+			max=255,
+		},
+		["Blue"] = {
+			id="cross_hud_color_b",
+			default="255",
+			help="Change the amount of blue in the crosshair",
+			min=0,
+			max=255,
+		},
+		["Alpha"] = {
+			id="cross_hud_color_b",
+			default="255",
+			help="Change the transparency of the crosshair",
+			min=0,
+			max=255,
+		},
+
+		["TargetRed"] = {
+			id="target_cross_hud_color_r",
+			default="255",
+			help="Change the amount of red in the crosshair when aiming at a target",
+			min=0,
+			max=255,
+		},
+		["TargetGreen"] = {
+			id="target_cross_hud_color_g",
+			default="0",
+			help="Change the amount of green in the crosshair when aiming at a target",
+			min=0,
+			max=255,
+		},
+		["TargetBlue"] = {
+			id="target_cross_hud_color_b",
+			default="0",
+			help="Change the amount of blue in the crosshair when aiming at a target",
+			min=0,
+			max=255,
+		},
+		["TargetAlpha"] = {
+			id="target_cross_hud_color_a",
+			default="255",
+			help="Change the transparency of the crosshair when aiming at a target",
+			min=0,
+			max=255,
+		},
+
+		["Gap"] = {
+			id="cross_gap",
+			default="5",
+			help="Change size of the gap in the middle of the crosshair",
+			min=0,
+			max=50,
+		},
+		["Length"] = {
+			id="cross_length",
+			default="13",
+			help="Change the length of the lines in the crosshair",
+			min=0,
+			max=50,
+		},
+		["Thickness"] = {
+			id="cross_thickness",
+			default="1",
+			help="Change the length of the lines in the crosshair",
+			min=0,
+			max=50,
+		},
+		["UseArrow"] = {
+			id="cross_arrow",
+			default="1",
+			help="Change the lines on the crosshair to be pointed",
+			isBool=true,
+		},
+		["Stretch"] = {
+			id="cross_stretch",
+			default="0",
+			help="The amount to stretch the crosshair by",
+			min=0,
+			max=360,
+		},
+
+		["UseCircle"] = {
+			id="cross_circle",
+			default="0",
+			help="Add a circle to the middle of the crosshair",
+			isBool=true,
+		},
+		["CircleRadius"] = {
+			id="cross_radius",
+			default="0",
+			help="The radius for the circle if enabled with cross_circle",
+			min=0,
+			max=50,
+		},
+		["CircleSegments"] = {
+			id="cross_segments",
+			default="0",
+			help="The number of segments for the circle if enabled with cross_circle",
+			min=0,
+			max=50,
+		},
+		["DynamicSize"] = {
+			id="hc_dynamic_amount",
+			default="50",
+			help="The amount the dynamic crosshair will move when enabled with hc_dynamic_cross",
+			min=0,
+			max=50,
+		},
+	})
+
+
+
+	hook.Add("CrosshairDesigner_ValueChanged", "test", print)
 end
+
