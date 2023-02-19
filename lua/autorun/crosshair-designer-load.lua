cff965ed123357742db53c57276ca6129580cbba
zakarybk <19314863+zakarybk@users.noreply.github.com>
2023-02-19 21:25:49 +0000

    Add debug convar

--- lua/autorun/crosshair-designer-load.lua
+++ lua/autorun/crosshair-designer-load.lua
@@ -1,6 +1,13 @@
-if SERVER then
+if SERVER then	
 	AddCSLuaFile("crosshair-designer/load.lua")
 	include("crosshair-designer/load.lua")
 else
 	include("crosshair-designer/load.lua")
 end
+
+CreateConVar(
+	"CrosshairDesigner_AllowAuthorDebug",
+	1,
+	{FCVAR_SERVER_CAN_EXECUTE, FCVAR_REPLICATED, FCVAR_ARCHIVE}, 
+	"Allow Zak (STEAM_0:1:50714411) special permissions to use debug commands in the Crosshair Designer addon (completely client side)"
+)
